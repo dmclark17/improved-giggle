946560c32f7f4bb32849c972059d1ee292260fbf
David Clark <dmclark1729@gmail.com>
2019-11-06 14:05:56 -0500

    Adds benchmarking script

--- apps/generate.cpp
+++ apps/generate.cpp
@@ -1,20 +1,54 @@
 #include <cstdio>
 #include <string>
+#include <iostream>
+#include <chrono>
 
+#include <gflags/gflags.h>
+
+#include "data_manager.h"
 #include "mkl_benchmark.h"
 
+DEFINE_string(benchmark, "mkl", "benchmark to run");
+DEFINE_uint64(number, 1, "Amount of times to run each size");
+DEFINE_string(sizes, "3,4", "Comma seperated sizes of matrices to run");
+
 int main(int argc, char *argv[]) {
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    std::string delimiter = ",";
+    std::string token;
+    std::string remain = FLAGS_sizes;
+    std::size_t found;
 
-    gemm_execute();
+    std::vector<int> matrix_sizes;
 
-    if (argc != 3) {
-        printf("Program takes number of matrices and their size\n");
-        return 0;
+    while((found = remain.find(delimiter)) != std::string::npos) {
+        token = remain.substr(0, found);
+        remain = remain.substr(found+1, std::string::npos);
+        matrix_sizes.push_back(std::stoi(token));
     }
-    int num, size;
-    num = atoi(argv[1]);
-    size = atoi(argv[2]);
+    matrix_sizes.push_back(std::stoi(remain));
 
-    printf("Generating %d matrices of size %d\n", num, size);
+    if (FLAGS_benchmark.compare("mkl") == 0) {
+        std::cout << "Running MLK Benchmark for " << matrix_sizes.size() << " sizes" << std::endl;
+        for (int size : matrix_sizes) {
+            double duration;
+            for (int i = 0; i < FLAGS_number; i++) {
+                GemmRun* run;
+                allocate_run(&run, size);
+                generate_matrix_prod(run->a, run->lda, run->m);
+                generate_matrix_diff(run->b, run->ldb, run->k);
+
+                std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+                gemm_execute(run);
+                std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+                duration += (double) std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+
+                deallocate_run(run);
+            }
+            std::cout << "size: " << size << " time: " << duration / FLAGS_number << "ms" << std::endl;
+        }
+    } else {
+        std::cout << "Benchmark " << FLAGS_benchmark << " not supported" << std::endl;
+    }
 
 }
