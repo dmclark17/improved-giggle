d3a93b07cf1822361952d238bee00dd1c54df1ef
David Clark <dmclark1729@gmail.com>
2019-11-06 11:33:27 -0500

    Uses generation script in MKL benchmark

--- cpu-giggle/mkl_benchmark.cpp
+++ cpu-giggle/mkl_benchmark.cpp
@@ -3,66 +3,25 @@
 
 #include "mkl.h"
 
+#include "data_manager.h"
 
-void gemm_execute() {
+#include "mkl_benchmark.h"
+
+void gemm_execute(GemmRun* run) {
     CBLAS_LAYOUT layout;
     CBLAS_TRANSPOSE transa, transb;
-    MKL_INT         m, n, k;
-    MKL_INT         lda, ldb, ldc;
-    float           alpha, beta;
-    float          *a, *b, *c;
 
     layout = CblasRowMajor;
     transa = CblasNoTrans;
     transb = CblasNoTrans;
 
-    m = 4;
-    n = 4;
-    k = 4;
 
-    // let's assume row major format for now
-    lda = k;
-    ldb = n;
-    ldc = n;
 
-    alpha = 1.0;
-    beta = 0.0;
+    cblas_sgemm(layout, transa, transb,
+                (MKL_INT) run->m, (MKL_INT) run->n, (MKL_INT) run->k,
+                run->alpha,
+                run->a, (MKL_INT) run->lda,
+                run->b, (MKL_INT) run->ldb, run->beta,
+                run->c, (MKL_INT) run->ldc);
 
-    a = (float *)calloc( lda*m, sizeof(float) );
-    b = (float *)calloc( ldb*k, sizeof(float) );
-    c = (float *)calloc( ldc*m, sizeof(float) );
-
-    if ( a == NULL || b == NULL || c == NULL ) {
-        printf("\n Can't allocate memory arrays");
-        return;
-    }
-
-    for (MKL_INT i = 0; i < m; i++) {
-        for (MKL_INT j = 0; j < k; j++) {
-            a[i * lda + j] = i * j;
-        }
-    }
-
-    for (MKL_INT i = 0; i < k; i++) {
-        for (MKL_INT j = 0; j < n; j++) {
-            b[i * ldb + j] = i - j;
-        }
-    }
-
-    cblas_sgemm(layout, transa, transb, m, n, k, alpha,
-                a, lda,
-                b, ldb, beta,
-                c, ldc);
-
-    printf("OUTPUT DATA\n");
-    for (MKL_INT i = 0; i < m; i++) {
-        for (MKL_INT j = 0; j < n; j++) {
-            printf("%f ", c[i * ldc + j]);
-        }
-        printf("\n");
-    }
-
-    free(a);
-    free(b);
-    free(c);
-    }
+}
--- cpu-giggle/mkl_benchmark.h
+++ cpu-giggle/mkl_benchmark.h
@@ -1,6 +1,9 @@
 #ifndef _IGIGGLE_MKL_H_
 #define _IGIGGLE_MKL_H_
 
-void gemm_execute();
+#include "data_manager.h"
+
+
+void gemm_execute(GemmRun* run);
 
 #endif /* end of include guard: _IGIGGLE_MKL_H_ */
