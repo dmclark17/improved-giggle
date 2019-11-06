63bc06e6bc26abc491487e9d5241fcb77c7ac719
David Clark <dmclark1729@gmail.com>
2019-11-06 11:32:45 -0500

    Generates matices in data manager

---
+++ src/data_manager.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "data_manager.h"
+
+
+int allocate_run(GemmRun** run, int size) {
+    (*run) = new GemmRun;
+
+    (*run)->m = size;
+    (*run)->n = size;
+    (*run)->k = size;
+
+    // let's assume row major format for now. We need to look into padding
+    (*run)->lda = (*run)->k;
+    (*run)->ldb = (*run)->n;
+    (*run)->ldc = (*run)->n;
+
+    (*run)->alpha = 1.0;
+    (*run)->beta = 0.0;
+
+    (*run)->a = (float *)calloc( (*run)->lda * (*run)->m, sizeof(float) );
+    (*run)->b = (float *)calloc( (*run)->ldb * (*run)->k, sizeof(float) );
+    (*run)->c = (float *)calloc( (*run)->ldc * (*run)->m, sizeof(float) );
+
+    if ( (*run)->a == NULL || (*run)->b == NULL || (*run)->c == NULL ) {
+        printf("\n Can't allocate memory arrays");
+        return 1;
+    }
+
+    return 0;
+}
+
+void generate_matrix_prod(float* mat, int ld, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            mat[i * ld + j] = i * j;
+        }
+    }
+}
+
+void generate_matrix_diff(float* mat, int ld, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            mat[i * ld + j] = i - j;
+        }
+    }
+}
+
+void deallocate_run(GemmRun* run) {
+    free(run->a);
+    free(run->b);
+    free(run->c);
+
+    delete run;
+}
--- src/data_manager.h
+++ src/data_manager.h
@@ -1,6 +1,28 @@
 #ifndef _IGIGGLE_DM_H_
 #define _IGIGGLE_DM_H_
 
-void generate_matrix()
+typedef struct GemmRun {
+    int m;
+    int n;
+    int k;
+    float* a;
+    float* b;
+    float* c;
+    int lda;
+    int ldb;
+    int ldc;
+    float alpha;
+    float beta;
+} GemmRun;
+
+int allocate_run(GemmRun** run, int size);
+
+void generate_matrix_prod(float* mat, int ld, int n);
+
+void generate_matrix_diff(float* mat, int ld, int n);
+
+void generate_matrix_random(float* mat, int ld, int n);
+
+void deallocate_run(GemmRun* run);
 
 #endif /* end of include guard: _IGIGGLE_DM_H_ */
