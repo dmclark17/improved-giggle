47a8969d560e4a046087e4ca5068d8013fe579da
David Clark <dmclark1729@gmail.com>
2019-11-08 15:11:02 -0500

    Makes MKL optional

--- cpu-giggle/mkl_benchmark.cpp
+++ cpu-giggle/mkl_benchmark.cpp
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _IG_HASMKL
 #include "mkl.h"
+#endif
 
 #include "data_manager.h"
 
 #include "cpu_benchmark.h"
 
 void mkl_gemm_execute(GemmRun* run) {
+#ifdef _IG_HASMKL
     CBLAS_LAYOUT layout;
     CBLAS_TRANSPOSE transa, transb;
 
@@ -15,13 +18,14 @@ void mkl_gemm_execute(GemmRun* run) {
     transa = CblasNoTrans;
     transb = CblasNoTrans;
 
-
-
     cblas_sgemm(layout, transa, transb,
                 (MKL_INT) run->m, (MKL_INT) run->n, (MKL_INT) run->k,
                 run->alpha,
                 run->a, (MKL_INT) run->lda,
                 run->b, (MKL_INT) run->ldb, run->beta,
                 run->c, (MKL_INT) run->ldc);
-
+#else
+    (void) run;
+    printf("MKL not supported\n");
+#endif
 }
