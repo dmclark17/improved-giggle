261a9f2fbd99ec4de04b625f8862feb0229666c5
David Clark <dmclark1729@gmail.com>
2019-11-13 15:38:25 -0500

    Adds larger tests comparing MKL and naive

--- tests/naive_test.cpp
+++ tests/naive_test.cpp
@@ -25,10 +25,44 @@ TEST(NaiveTests, ThreeByThree) {
 
     for (int i = 0; i < run->m; i++) {
         for (int j = 0; j < run->n; j++) {
-            ASSERT_TRUE(run->c[i * run->ldc + j] == expected[i * run->ldc + j]);
+            ASSERT_NEAR(run->c[i * run->ldc + j], expected[i * run->ldc + j],
+                        1e-10);
         }
     }
 
     free(expected);
     deallocate_run(run);
+}
+
+TEST(NaiveTests, BigTest) {
+    GemmRun* run;
+    GemmRun* run_mkl;
+    allocate_run(&run, 512);
+    allocate_run(&run_mkl, 512);
+
+    generate_matrix_random(run->a, run->lda, run->m);
+    generate_matrix_random(run->b, run->ldb, run->k);
+
+    // to free laters
+    float* temp_a = run_mkl->a;
+    float* temp_b = run_mkl->b;
+
+    run_mkl->a = run->a;
+    run_mkl->b = run->b;
+
+    mkl_gemm_execute(run_mkl);
+    naiveCPU_gemm_execute(run);
+
+    for (int i = 0; i < run->m; i++) {
+        for (int j = 0; j < run->n; j++) {
+            ASSERT_NEAR(run->c[i * run->ldc + j], run_mkl->c[i * run->ldc + j],
+                        1e-3);
+        }
+    }
+
+    run_mkl->a = temp_a;
+    run_mkl->b = temp_b;
+
+    deallocate_run(run);
+    deallocate_run(run_mkl);
 }
