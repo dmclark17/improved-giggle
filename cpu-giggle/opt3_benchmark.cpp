0321564b0a3a9c891c7287a44b42fed2ee96ba60
David Clark <dmclark1729@gmail.com>
2019-11-21 17:11:45 -0500

    Adds blocking???

--- cpu-giggle/opt3_benchmark.cpp
+++ cpu-giggle/opt3_benchmark.cpp
@@ -8,8 +8,8 @@
 #include "cpu_benchmark.h"
 
 
-#define NC 256
-#define KC 512
+#define NC 128
+#define KC 1024
 #define MR 8
 #define NR 8
 
@@ -143,18 +143,35 @@ inline void opt3CPU_aux_simd(float* a_pack, float* b_pack, float* c_pack) {
     }
     #else
     #ifdef __AVX__
-    __m256 a, b, c;
+    __m256 a, b, c, c1, c2, c3, a1, a2, a3;
     unsigned int pack_n, pack_i, pack_j, pack_z;
+    pack_j = 0;
     for (pack_n = 0; pack_n < NC; pack_n += NR) {
         for (pack_j = 0; pack_j < NR; pack_j += 8) {
-            for (pack_z = 0; pack_z < KC; pack_z++) {
-                b = _mm256_load_ps(b_pack + (pack_n * KC) + (NR * pack_z) + pack_j);
-                for (pack_i = 0; pack_i < MR; pack_i++) {
-                    c = _mm256_load_ps(c_pack + pack_n + (pack_i * NC) + pack_j);
+            for (pack_i = 0; pack_i < MR; pack_i +=4) {
+                c = _mm256_load_ps(c_pack + pack_n + (pack_i * NC) + pack_j);
+                c1 = _mm256_load_ps(c_pack + pack_n + ((pack_i+1) * NC) + pack_j);
+                c2 = _mm256_load_ps(c_pack + pack_n + ((pack_i+2) * NC) + pack_j);
+                c3 = _mm256_load_ps(c_pack + pack_n + ((pack_i+3) * NC) + pack_j);
+                for (pack_z = 0; pack_z < KC; pack_z++) {
+                    b = _mm256_load_ps(b_pack + (pack_n * KC) + (NR * pack_z) + pack_j);
+
+                    
                     a = _mm256_broadcast_ss(a_pack + (pack_i * KC) + pack_z);
+                    a1 = _mm256_broadcast_ss(a_pack + ((pack_i+1) * KC) + pack_z);
+                    a2 = _mm256_broadcast_ss(a_pack + ((pack_i+2) * KC) + pack_z);
+                    a3 = _mm256_broadcast_ss(a_pack + ((pack_i+3) * KC) + pack_z);
+
+
                     c = _mm256_fmadd_ps(a, b, c);
-                    _mm256_store_ps(c_pack + pack_n + pack_i * NC + pack_j, c);
+                    c1 = _mm256_fmadd_ps(a1, b, c1);
+                    c2 = _mm256_fmadd_ps(a2, b, c2);
+                    c3 = _mm256_fmadd_ps(a3, b, c3);
                 }
+                _mm256_store_ps(c_pack + pack_n + pack_i * NC + pack_j, c);
+                _mm256_store_ps(c_pack + pack_n + (pack_i+1) * NC + pack_j, c1);
+                _mm256_store_ps(c_pack + pack_n + (pack_i+2) * NC + pack_j, c2);
+                _mm256_store_ps(c_pack + pack_n + (pack_i+3) * NC + pack_j, c3);
             }
         }
     }
