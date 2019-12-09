64d2a4bcd7bf4e8e87210076f202eeec22687cec
David Clark <dmclark1729@gmail.com>
2019-12-09 10:48:05 -0500

    Adds side by side plotting

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -10,7 +10,7 @@ def parse_output(filename, results={}):
 
         for line in f:
             line_list = line.strip().split(" ")
-            result_list.append((int(line_list[1]), int(line_list[3])))
+            result_list.append((float(line_list[1]), float(line_list[3])))
 
     return results
 
@@ -23,14 +23,70 @@ def plot_output(results):
 
     plt.xlabel("Matrix Size")
     plt.ylabel("Time (ms)")
-    plt.title("Matrix-matrix multiplication benchmark (O0)")
+    plt.title("GPU Matrix-matrix multiplication benchmark")
     plt.legend()
-    plt.savefig("CPUO0.png")
+    plt.savefig("gpu.pdf")
+
+
+def plot_double_output(results_one, results_two):
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(1, 2, 1)
+    for key in results_one:
+        plt.plot([x[0] for x in results[key]], [x[1] for x in results[key]],
+                 label=key)
+
+    plt.xlabel("Matrix Size")
+    plt.ylabel("Time (ms)")
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    for key in results_two:
+        plt.plot([x[0] for x in results_two[key]], [x[1] for x in results_two[key]],
+                 label=key)
+
+    plt.xlabel("Matrix Size")
+    plt.ylabel("Time (ms)")
+    plt.legend()
+
+    plt.savefig("cpu_new.pdf")
 
 
 if __name__ == "__main__":
-    results = parse_output("../build/naiveCPU.txt")
-    results = parse_output("../build/opt1CPU.txt", results=results)
-    results = parse_output("../build/opt2CPU.txt", results=results)
+    """
+    ./apps/benchmark --number 4 --sizes 512,512,1024,1536,2048 --benchmark naiveCPU > naiveCPU_results.txt
+    ./apps/benchmark --number 4 --sizes 512,512,1024,1536,2048 --benchmark opt1CPU > opt1CPU_results.txt
+    ./apps/benchmark --number 4 --sizes 512,512,1024,1536,2048 --benchmark opt2CPU > opt2CPU_results_short.txt
+    ./apps/benchmark --number 4 --sizes 512,512,1024,1536,2048,2560,3072,3584 --benchmark mkl > mkl_one_results.txt
+    ./apps/benchmark --number 4 --sizes 512,512,1024,1536,2048,2560,3072,3584 --benchmark opt2CPU > opt2CPU_results.txt
+    ./apps/benchmark --number 4 --sizes 512,512,1024,1536,2048,2560,3072,3584 --benchmark opt3CPU > opt3CPU_results.txt
+
+
+    ./apps/benchmark --number 4 --sizes 512,512,1024,1536,2048,2560,3072,3584 --benchmark naiveGPU > naiveGPU_results.txt
+    ./apps/benchmark --number 4 --sizes 512,512,1024,1536,2048,2560,3072,3584 --benchmark opt1GPU > opt1GPU_results.txt
+    ./apps/benchmark --number 4 --sizes 512,512,1024,1536,2048,2560,3072,3584 --benchmark opt2GPU > opt2GPU_results.txt
+    ./apps/benchmark --number 4 --sizes 512,512,1024,1536,2048,2560,3072,3584 --benchmark cublas > cublas_results.txt
+    ./apps/benchmark --number 4 --sizes 512,512,1024,1536,2048,2560,3072,3584 --benchmark mkl > mkl_four_results.txt
+    """
+
+    # results = parse_output("../build/mkl_four_results.txt", results={})
+    # results = parse_output("../build/naiveGPU_results.txt", results=results)
+    # results = parse_output("../build/opt1GPU_results.txt", results=results)
+    # results = parse_output("../build/opt2GPU_results.txt", results=results)
+    # results = parse_output("../build/cublas_results.txt", results=results)
+    #
+    # plot_output(results)
+
+
+
+
+    results = parse_output("../build/naiveCPU_results.txt", results={})
+    results = parse_output("../build/opt1CPU_results.txt", results=results)
+    results = parse_output("../build/opt2CPU_results_short.txt", results=results)
+
+    results_two = parse_output("../build/mkl_one_results.txt", results={})
+    results_two = parse_output("../build/opt2CPU_results.txt", results=results_two)
+    results_two = parse_output("../build/opt3CPU_results.txt", results=results_two)
     print(results)
-    plot_output(results)
+    print(results_two)
+    plot_double_output(results, results_two)
